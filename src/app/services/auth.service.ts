9c0e9d8373e47db2d986e99fc6811b925d6c587b
Gladkowski <marcingladkowski@gmail.com>
2018-06-23 11:30:45 +0200

    add auth service to login modal form

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+@Injectable()
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  headers = new HttpHeaders({
+    'Content-Type': 'application/json'
+  });
+
+  login(user) {
+    return this.http.post(`http://localhost:8080/api/auth`, user, {headers: this.headers});
+  }
+
+}
