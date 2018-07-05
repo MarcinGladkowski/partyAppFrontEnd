2a0a69906a9fad9a2313f8b5a0790201f25fedc7
Gladkowski <marcingladkowski@gmail.com>
2018-07-06 00:17:11 +0200

    close login modal when login properly

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef, Input, AfterViewInit} from '@
 import { FormGroup, FormControl } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 import { ModalDirective } from 'angular-bootstrap-md';
-
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-login',
@@ -12,6 +12,7 @@ import { ModalDirective } from 'angular-bootstrap-md';
 export class LoginComponent implements OnInit {
 
   public loginForm: FormGroup;
+  isLoggedIn: Observable<boolean>;
 
   @ViewChild('modal') public modalRef: ModalDirective;
 
@@ -21,10 +22,24 @@ export class LoginComponent implements OnInit {
     this.modalRef.show();
   }
 
+  closeModal() {
+    this.modalRef.hide();
+  }
+
   ngOnInit() {
+
     this.loginForm = new FormGroup({
       email: new FormControl(),
       password: new FormControl(),
+    });
+
+    this.authService.isLoggedIn().subscribe((data: any) => {
+      console.log(`logged in login: ${data}`);
+      this.isLoggedIn = data;
+      if (data) {
+         this.closeModal();
+      }
+
     });
   }
 
