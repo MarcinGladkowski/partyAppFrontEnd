386928ff853caef292e73d5912631e54523b3a07
Gladkowski <marcingladkowski@gmail.com>
2018-09-05 21:31:48 +0200

    add party routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,11 +2,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { SettingsComponent } from './settings/settings.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { PartyComponent } from './party/party.component';
 
 const routesConfig: Routes = [
   {path: '', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'settings', component: SettingsComponent}
+  {path: 'settings', component: SettingsComponent},
+  {path: 'party', component: PartyComponent}
 ];
 
 export const routerModule = RouterModule.forRoot(routesConfig);
