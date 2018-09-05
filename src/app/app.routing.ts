63b8c0582761ade761159a2b4969402c70953fa5
Gladkowski <marcingladkowski@gmail.com>
2018-09-05 22:11:11 +0200

    add components to routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,12 +3,15 @@ import { SettingsComponent } from './settings/settings.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { PartyComponent } from './party/party.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routesConfig: Routes = [
   {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'settings', component: SettingsComponent},
-  {path: 'party', component: PartyComponent}
+  {path: 'party', component: PartyComponent},
+  {path: 'admin', component: AdminComponent}
 ];
 
 export const routerModule = RouterModule.forRoot(routesConfig);
