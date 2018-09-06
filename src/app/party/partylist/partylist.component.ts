5a5fb0b2f94703641ceda9262aa8430cf6a91ca3
Marcin Gładkowski <mgladkowski@swift.pl>
2018-09-06 11:11:51 +0200

    create partylist component

---
+++ src/app/party/partylist/partylist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-partylist',
+  templateUrl: './partylist.component.html',
+  styleUrls: ['./partylist.component.css']
+})
+export class PartylistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
