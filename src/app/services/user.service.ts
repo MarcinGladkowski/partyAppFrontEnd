c0ca04955c36be6eb918cfd5da0bf7b5527e5fb6
Gladkowski <marcingladkowski@gmail.com>
2018-09-06 22:08:44 +0200

    post method to activate user

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -10,8 +10,21 @@ export class UserService {
     'Content-Type': 'application/json'
   });
 
+  /**
+   * Send data to register user in db
+   * @param user
+   */
   register(user) {
     return this.http.post(`http://localhost:8080/api/users`, user, {headers: this.headers})
+    .subscribe(data => console.log(data));
+  }
+
+  /**
+   * activate user from register email
+   * @param hash
+   */
+  activate(hash) {
+    return this.http.post(`http://localhost:8080/api/users/activate`, hash, {headers: this.headers})
     .subscribe(data => console.log(data));
   }
 }
