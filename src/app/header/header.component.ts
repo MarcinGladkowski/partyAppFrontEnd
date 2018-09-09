5f198a48330e5d8da5890ff63f80c61524968cf8
Gladkowski <marcingladkowski@gmail.com>
2018-09-09 09:38:18 +0200

    Redirect after logout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter} from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -12,23 +13,29 @@ export class HeaderComponent implements OnInit {
   isLoggedIn;
 
   logout() {
-    this.authService.logout();
+    this.redirectAfterLogout();
   }
 
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ) {}
 
 
   ngOnInit() {
 
-    console.log(`logged in header: ${this.isLoggedIn}`);
+    console.log(`logged in header (on init) : ${this.isLoggedIn}`);
 
     this.authService.isLoggedIn().subscribe((data: any) => {
-      console.log(`logged in header: ${data}`);
+      console.log(`logged in header (after method): ${data}`);
       this.isLoggedIn = data;
     });
 
+  }
+
+  redirectAfterLogout() {
+    this.authService.logout();
+    this.router.navigate(['/']);
   }
 
 }
