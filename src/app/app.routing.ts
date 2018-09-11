94adaa1366fc0e701d386fcb8cf4478a1f4f5485
Marcin Gładkowski <mgladkowski@swift.pl>
2018-09-11 21:37:58 +0200

    register router for type component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,6 +7,7 @@ import { PartylistComponent } from './party/partylist/partylist.component';
 import { PartymainComponent } from './party/partymain/partymain.component';
 import { AdminComponent } from './admin/admin.component';
 import { ActionComponent } from './action/action.component';
+import { PartyTypeComponent } from './admin/party-type/party-type.component';
 
 const routesConfig: Routes = [
   {path: '', component: LoginComponent},
@@ -18,8 +19,11 @@ const routesConfig: Routes = [
     {path: 'add', component: PartyComponent },
     {path: 'list', component: PartylistComponent },
   ]},
-  {path: 'action/activate/:hash', component: ActionComponent},
-  {path: 'admin', component: AdminComponent}
+  {path: 'admin', component: AdminComponent, children: [
+    {path: '', redirectTo: 'users', pathMatch: 'full'},
+    {path: 'type', component: PartyTypeComponent },
+  ]},
+  {path: 'action/activate/:hash', component: ActionComponent}
 ];
 
 export const routerModule = RouterModule.forRoot(routesConfig);
