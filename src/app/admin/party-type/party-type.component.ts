41e9ffe5afea6a05a27f02b1528bdfb1b95cde41
Marcin Gładkowski <mgladkowski@swift.pl>
2018-09-12 20:35:23 +0200

    send req from component

--- src/app/admin/party-type/party-type.component.ts
+++ src/app/admin/party-type/party-type.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl,  Validators, FormBuilder } from '@angular/forms';
+import { PartyTypeService } from '../../services/party-type.service';
 
 @Component({
   selector: 'app-party-type',
@@ -12,6 +13,7 @@ export class PartyTypeComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
+    private partyTypeService: PartyTypeService
   ) { }
 
   ngOnInit() {
@@ -22,6 +24,12 @@ export class PartyTypeComponent implements OnInit {
       'icon': [null, Validators.required],
     });
 
+  }
+
+  onSubmit() {
+    this.partyTypeService.create(JSON.stringify(this.partyTypeForm.value)).subscribe((data: any) => {
+      console.log(data);
+    });
   }
 
 }
