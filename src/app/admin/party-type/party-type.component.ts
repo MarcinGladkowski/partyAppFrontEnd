df7db057aa6de1993fe7e22126a62fb98f83b2c3
Gladkowski <marcingladkowski@gmail.com>
2018-09-23 22:36:49 +0200

    working on upload file by form

--- src/app/admin/party-type/party-type.component.ts
+++ src/app/admin/party-type/party-type.component.ts
@@ -21,15 +21,33 @@ export class PartyTypeComponent implements OnInit {
     this.partyTypeForm = this.formBuilder.group({
       'name': [null, Validators.required],
       'desc': [null, Validators.required],
-      'icon': [null, Validators.required],
+      'icon': ['', Validators.required]
     });
 
   }
 
   onSubmit() {
-    this.partyTypeService.create(JSON.stringify(this.partyTypeForm.value)).subscribe((data: any) => {
-      console.log(data);
-    });
+
+    console.log(this.partyTypeForm.value);
+
+    // this.partyTypeService.create(JSON.stringify(this.partyTypeForm.value)).subscribe((data: any) => {
+    //   console.log(data);
+    // });
+  }
+
+  onFileChange(event) {
+    const reader = new FileReader();
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+
+        this.partyTypeForm.get('icon').setValue(file ? file.name : '');
+
+
+      };
+    }
   }
 
 }
