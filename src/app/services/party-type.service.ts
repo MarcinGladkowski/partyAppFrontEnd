5831043ffd960c124383acf4911fabe36d13437f
Gladkowski <marcingladkowski@gmail.com>
2018-09-24 21:52:37 +0200

    method to upload file

--- src/app/services/party-type.service.ts
+++ src/app/services/party-type.service.ts
@@ -10,8 +10,19 @@ export class PartyTypeService {
     'Content-Type': 'application/json'
   });
 
+  headersFile = new HttpHeaders({
+    'Content-Type': 'multipart/form-data'
+  });
+
   create(partyType) {
     return this.http.post(`http://localhost:8080/api/party-type`, partyType, {headers: this.headers});
+  }
+
+  upload(file: any) {
+    const input = new FormData();
+    input.append('file', file);
+
+    return this.http.post(`http://localhost:8080/api/party-type/upload`, input, {headers: this.headersFile} );
   }
 
 }
