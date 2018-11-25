cf30abd39dc520bd2bb49e18cad9078b20abeb2e
Gladkowski <marcingladkowski@gmail.com>
2018-11-25 20:04:34 +0100

    return header config

--- src/app/services/party-type.service.ts
+++ src/app/services/party-type.service.ts
@@ -10,19 +10,14 @@ export class PartyTypeService {
     'Content-Type': 'application/json'
   });
 
-  headersFile = new HttpHeaders({
-    'Content-Type': 'multipart/form-data'
-  });
-
   create(partyType) {
     return this.http.post(`http://localhost:8080/api/party-type`, partyType, {headers: this.headers});
   }
 
   upload(file: any) {
-    const input = new FormData();
-    input.append('file', file);
+    const formData = new FormData();
+    formData.append('icon', file, file.name);
 
-    return this.http.post(`http://localhost:8080/api/party-type/upload`, input, {headers: this.headersFile} );
+    return this.http.post(`http://localhost:8080/api/party-type/upload`, formData);
   }
-
 }
