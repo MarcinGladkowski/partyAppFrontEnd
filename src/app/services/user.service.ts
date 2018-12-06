b910711176d09c984bb9d95349a649e2ac0255ce
Gladkowski <marcingladkowski@gmail.com>
2018-12-06 23:19:23 +0100

    method to get details from user

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,9 @@
+import { UserAttrs } from './../user/user-attrs';
 import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
+import { User } from '../user/user';
 
 @Injectable()
 export class UserService {
@@ -20,5 +23,14 @@ export class UserService {
    */
   activate(hash) {
     return this.http.post(`${environment.api}/users/activate`, hash);
+  }
+
+  /**
+   * Method to get user data by id
+   */
+  getUser() {
+    return this.http.get<UserAttrs>(`${environment.api}/users`).pipe(
+     map((userAttrs) => new User(userAttrs))
+    );
   }
 }
