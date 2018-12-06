f5c3ab234626a522c8bfb2feec245f8ee87d019d
Gladkowski <marcingladkowski@gmail.com>
2018-12-06 23:19:50 +0100

    create parent component, profile form

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,4 +1,7 @@
+import { UserService } from './../../services/user.service';
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  profileForm: FormGroup;
+
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+
+    this.profileForm = new FormGroup({
+      username: new FormControl(),
+      email: new FormControl(),
+    });
+
+    this.userService.getUser().subscribe((user: User) => {
+      this.profileForm.controls['username'].setValue(user.username);
+      this.profileForm.controls['email'].setValue(user.email);
+    });
+  }
+
+  onSubmit() {
+    console.log(this.profileForm.value);
   }
 
 }
