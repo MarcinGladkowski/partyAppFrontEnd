4601d145c8619e8a60aa2bce8aa0fa8f00f553ed
Gladkowski <marcingladkowski@gmail.com>
2018-12-08 23:22:54 +0100

    refactoring routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,29 +6,31 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { PartyComponent } from './party/party.component';
 import { PartylistComponent } from './party/partylist/partylist.component';
-import { PartymainComponent } from './party/partymain/partymain.component';
-import { AdminComponent } from './admin/admin.component';
 import { ActionComponent } from './action/action.component';
 import { PartyTypeComponent } from './party/party-type/party-type.component';
+import {PartyFormComponent} from './party/party-form/party-form.component';
+import {PartyDetailsComponent} from './party/party-details/party-details.component';
 
 const routesConfig: Routes = [
   {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'settings', component: SettingsComponent},
-  {path: 'party', component: PartymainComponent, children: [
-    {path: '', redirectTo: 'list', pathMatch: 'full'},
-    {path: 'add', component: PartyComponent },
-    {path: 'list', component: PartylistComponent },
-  ]},
-  {path: 'admin', component: AdminComponent, children: [
-    {path: '', redirectTo: 'type', pathMatch: 'full'},
-    {path: 'type', component: PartyTypeComponent },
-  ]},
-  {path: 'user', component: UserComponent, children: [
-    {path: '', redirectTo: 'profile', pathMatch: 'full'},
-    {path: 'profile', component: ProfileComponent },
-  ]},
+  {
+    path: 'party', component: PartyComponent, children: [
+      {path: '', redirectTo: 'list', pathMatch: 'full'},
+      {path: 'add', component: PartyFormComponent},
+      {path: 'list', component: PartylistComponent},
+      {path: 'type', component: PartyTypeComponent},
+      {path: 'details/:id', component: PartyDetailsComponent},
+    ]
+  },
+  {
+    path: 'user', component: UserComponent, children: [
+      {path: '', redirectTo: 'profile', pathMatch: 'full'},
+      {path: 'profile', component: ProfileComponent},
+    ]
+  },
   {path: 'action/activate/:hash', component: ActionComponent}
 ];
 
