53fd2730ce3d89fa65a142af9a5c372f8717c700
Gladkowski <marcingladkowski@gmail.com>
2018-12-08 23:21:47 +0100

    Create party details component

---
+++ src/app/party/party-details/party-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-party-details',
+  templateUrl: './party-details.component.html',
+  styleUrls: ['./party-details.component.scss']
+})
+export class PartyDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
