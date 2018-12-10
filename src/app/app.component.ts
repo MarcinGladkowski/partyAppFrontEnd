09fe66afc38138ed980a1ca245c736de1639132e
Gladkowski <marcingladkowski@gmail.com>
2018-12-10 23:57:27 +0100

    remove subscribe to routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 /// <reference types="@types/googlemaps" />
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from './services/auth.service';
-import { Observable } from 'rxjs';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,18 +8,7 @@ import { Router } from '@angular/router';
 })
 export class AppComponent implements OnInit {
 
-  isLoggedIn: Observable<boolean>;
+  constructor() {}
 
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) {
-    this.router.events.subscribe((event) => {
-      this.authService.checkIsUserLogin();
-    });
-  }
-
-  ngOnInit() {
-    this.authService.checkIsUserLogin();
-  }
+  ngOnInit() {}
 }
