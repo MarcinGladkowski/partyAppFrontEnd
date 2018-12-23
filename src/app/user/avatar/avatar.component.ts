fde2de3f64f42640cfee6f7bcd99d3cad2f22efe
Gladkowski <marcingladkowski@gmail.com>
2018-12-23 22:56:57 +0100

    update user avatar component

--- src/app/user/avatar/avatar.component.ts
+++ src/app/user/avatar/avatar.component.ts
@@ -1,5 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
+import {UserService} from '../../services/user.service';
+import {User} from '../user';
+import {Form, FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-avatar',
@@ -8,14 +11,43 @@ import {AuthService} from '../../services/auth.service';
 })
 export class AvatarComponent implements OnInit {
 
-  user: any;
+  user: User;
+  fileUpload: File = null;
+  avatarForm: FormGroup;
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+
+    this.avatarForm = this.fb.group({});
+
     this.authService.getProfile().subscribe(user => {
       this.user = user;
     });
+  }
+
+  onSubmit() {
+    const form = { avatar: this.fileUpload };
+    this.userService.updateAvatar(form).subscribe((user: User) => {
+      console.log(user);
+    });
+  }
+
+  onFileChange(event) {
+    const reader = new FileReader();
+    if (event.target.files && event.target.files.length > 0) {
+
+      const file = event.target.files[0];
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+        this.fileUpload = file;
+      };
+    }
   }
 
 }
