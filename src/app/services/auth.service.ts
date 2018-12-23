499bdc5e7e4a78e9a4ffbbf20cbf1e360cdc0933
Gladkowski <marcingladkowski@gmail.com>
2018-12-23 22:57:28 +0100

    update password method service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,36 +1,40 @@
-import { map } from 'rxjs/operators';
-import { environment } from './../../environments/environment';
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { Subject, Observable, BehaviorSubject } from 'rxjs';
+import {map} from 'rxjs/operators';
+import {environment} from './../../environments/environment';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders, HttpErrorResponse} from '@angular/common/http';
+import {Subject, Observable, BehaviorSubject} from 'rxjs';
+import {User} from '../user/user';
+import {UserService} from './user.service';
 
 @Injectable()
 export class AuthService {
 
-  constructor(private http: HttpClient) {
-    //  this.loggedIn.next(!!localStorage.getItem('authToken'));
-     this.loggedIn.next(true);
-  }
-
-  headers = new HttpHeaders({
-    'Content-Type': 'application/json',
-    // 'Cache-Control': 'max-age=5'
-  });
+  constructor(
+    private http: HttpClient,
+    private userService: UserService
+    ) {}
 
   /** change loggedIn to a subject */
-  public loggedIn = new BehaviorSubject<boolean>(false);
-
+  loggedIn = new BehaviorSubject<boolean>(false);
+  /** user profile */
+  user = new BehaviorSubject<any>(false);
   /** make isLoggedIn public readonly */
   isLoggedIn(): Observable<boolean> {
     return this.loggedIn.asObservable();
   }
+  getProfile(): Observable<any> {
+    return this.user.asObservable();
+  }
 
-   login(user) {
-    return this.http.post(`${environment.api}/auth`, user, {headers: this.headers}).pipe(
+  login(loginData) {
+    return this.http.post(`${environment.api}/auth`, loginData).pipe(
       map((data: any) => {
         if (data && data.auth) {
           localStorage.setItem('auth', JSON.stringify(data));
           this.loggedIn.next(true);
+          this.userService.getUser().subscribe((user: User) => {
+            this.user.next(user);
+          });
         }
         return data;
       })
@@ -43,17 +47,23 @@ export class AuthService {
   }
 
   checkIsUserLogin() {
-    return this.http.post(`${environment.api}/auth/check`, null, {headers: this.headers})
-    .subscribe((data: any) => {
-      if (data && data.auth) {
-        localStorage.setItem('auth', JSON.stringify(data));
-        this.loggedIn.next(true);
-      }},
-      (err) => {
-        localStorage.setItem('auth', JSON.stringify({auth: false, token: null}));
-        this.loggedIn.next(false);
-      }
-    );
+    return this.http.post(`${environment.api}/auth/check`, null)
+      .subscribe((data: any) => {
+          if (data && data.auth) {
+            localStorage.setItem('auth', JSON.stringify(data));
+            this.loggedIn.next(true);
+          }
+        },
+        (err) => {
+          localStorage.setItem('auth', JSON.stringify({auth: false, token: null}));
+          this.loggedIn.next(false);
+        }
+      );
   }
 
+  updatePassword(data): Observable<User> {
+    return this.http.post(`${environment.api}/auth/update-password`, data).pipe(
+      map((userAttrs) => new User(userAttrs))
+    );
+  }
 }
