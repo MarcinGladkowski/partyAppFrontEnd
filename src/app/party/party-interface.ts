9807d993106f2ba594e24c58f9619c3ab8021b42
Gladkowski <marcingladkowski@gmail.com>
2018-12-24 00:36:19 +0100

    add fields to party interface

--- src/app/party/party-interface.ts
+++ src/app/party/party-interface.ts
@@ -1,6 +1,14 @@
+import {User} from '../user/user';
+import {PartyType} from './party-type/party-type';
+
 export interface PartyInterface {
     _id: string;
     name: string;
     desc: string;
-    type: string;
+    latitude: number;
+    longitude: number;
+    createdDate: string;
+    partyType: PartyType;
+    participants: User[];
+    userCreated: User;
 }
