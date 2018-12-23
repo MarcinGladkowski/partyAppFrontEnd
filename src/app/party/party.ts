632a982f514bc4b117f027ea5792fe7e583856bb
Gladkowski <marcingladkowski@gmail.com>
2018-12-24 00:36:48 +0100

    add fields and constructor to party class

--- src/app/party/party.ts
+++ src/app/party/party.ts
@@ -1,8 +1,27 @@
 import { PartyInterface } from './party-interface';
+import {User} from '../user/user';
+import {PartyType} from './party-type/party-type';
 
 export class Party implements PartyInterface {
   _id: string;
+  createdDate: string;
   desc: string;
+  latitude: number;
+  longitude: number;
   name: string;
-  type: string;
+  participants: User[];
+  partyType: PartyType;
+  userCreated: User;
+
+  constructor(attrs: Partial<PartyInterface>) {
+    this._id = attrs._id;
+    this.createdDate = attrs.createdDate;
+    this.desc = attrs.desc;
+    this.latitude = attrs.latitude;
+    this.longitude = attrs.longitude;
+    this.name = attrs.name;
+    this.participants = attrs.participants;
+    this.partyType = attrs.partyType;
+    this.userCreated = attrs.userCreated;
+  }
 }
