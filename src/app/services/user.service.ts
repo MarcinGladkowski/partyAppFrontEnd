4fc6609a05510a5f7673f0195dd4edcc04a8d9ea
Gladkowski <marcingladkowski@gmail.com>
2018-12-23 22:55:49 +0100

    user service method to update user profile

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,7 +2,7 @@ import { UserAttrs } from './../user/user-attrs';
 import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map, tap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { User } from '../user/user';
 
 @Injectable()
@@ -25,12 +25,20 @@ export class UserService {
     return this.http.post(`${environment.api}/users/activate`, hash);
   }
 
-  /**
-   * Method to get user data by id
-   */
+  /** Method to get user data by id */
   getUser() {
     return this.http.get<UserAttrs>(`${environment.api}/users`).pipe(
      map((userAttrs) => new User(userAttrs))
     );
+  }
+  update(formData) {
+    return this.http.put<UserAttrs>(`${environment.api}/users`, formData).pipe(
+      map((userAttrs) => new User(userAttrs))
+    );
+  }
+  updateAvatar(data) {
+    const formData = new FormData();
+    formData.append('avatar', data.avatar);
+    return this.http.post(`${environment.api}/update-avatar`, formData);
   }
 }
