5d517bd658a4b574393e5a014657c62f73fa4bef
Gladkowski <marcingladkowski@gmail.com>
2018-12-23 22:56:23 +0100

    create password form

--- src/app/user/password/password.component.ts
+++ src/app/user/password/password.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {PasswordRepeatValidator} from '../../validators/password-repeat-validator';
+import {AuthService} from '../../services/auth.service';
+import {User} from '../user';
 
 @Component({
   selector: 'app-password',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasswordComponent implements OnInit {
 
-  constructor() { }
+  updatePassword: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
+    this.updatePassword = this.fb.group({
+      oldPassword: new FormControl('', [Validators.required, Validators.min(3)]),
+      newPassword: new FormControl('', [Validators.required, Validators.min(3)]),
+      newPasswordRepeat: new FormControl('', [Validators.required, Validators.min(3)])
+    }, {validator: PasswordRepeatValidator });
   }
 
+  onSubmit() {
+    if (this.updatePassword.valid) {
+      this.authService.updatePassword(this.updatePassword.value).subscribe((user: User) => {
+        if (user) {
+          this.authService.user.next(user);
+        }
+      });
+    }
+  }
 }
