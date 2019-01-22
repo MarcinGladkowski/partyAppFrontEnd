d69a7699ce847c68147c1b7af41c2116cc8b2ff8
Marcin <marcingladkowski@gmail.com>
2019-01-23 00:16:17 +0100

    add password repeat validator

--- src/app/validators/password-repeat-validator.ts
+++ src/app/validators/password-repeat-validator.ts
@@ -5,3 +5,9 @@ export const PasswordRepeatValidator: ValidatorFn | null = (fg: FormGroup) => {
   const newPasswordRepeat = fg.get('newPasswordRepeat').value;
   return newPassword !== null && newPasswordRepeat !== null && newPassword === newPasswordRepeat ? null : { diff: true };
 };
+
+export const PasswordRegisterRepeatValidator: ValidatorFn | null = (fg: FormGroup) => {
+  const password = fg.get('password').value;
+  const repeatPassword = fg.get('repeatPassword').value;
+  return password !== null && repeatPassword !== null && password === repeatPassword ? null : { diff: true };
+};
