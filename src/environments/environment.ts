4838ac3ec0b5a253ac6ed38b88b7e1574f23a4b9
Marcin <marcingladkowski@gmail.com>
2019-01-27 19:11:26 +0100

    remove icons dir

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
   production: false,
-  iconsDir: 'assets/icon/',
   api: 'http://localhost:8080/api',
   upload: 'http://localhost:8080'
 };
